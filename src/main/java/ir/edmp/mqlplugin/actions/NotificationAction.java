2e1b19439921d175bcf58f9f419f7c200ecc85f1
m.parsapour <m.parsapour@imechpro.com>
2024-02-13 12:31:10 +0330

    Add classes for notification

---
+++ src/main/java/ir/edmp/mqlplugin/actions/NotificationAction.java
@@ -0,0 +1,12 @@
+package ir.edmp.mqlplugin.actions;
+
+import com.intellij.openapi.actionSystem.AnAction;
+import com.intellij.openapi.actionSystem.AnActionEvent;
+import org.jetbrains.annotations.NotNull;
+
+public class NotificationAction extends AnAction {
+    @Override
+    public void actionPerformed(@NotNull AnActionEvent e) {
+
+    }
+}
