9b1342b79fce897fb1aa1115e5a458c74d945092
m.parsapour <m.parsapour@plm.ir>
2024-03-07 04:23:10 +0500

    Update displaying results to use main thread

--- src/main/java/ir/edmp/mqlplugin/util/MessageViewUtil.java
+++ src/main/java/ir/edmp/mqlplugin/util/MessageViewUtil.java
@@ -1,12 +1,9 @@
 package ir.edmp.mqlplugin.util;
 
-import com.intellij.openapi.editor.Document;
-import com.intellij.openapi.fileEditor.FileEditorManager;
-import com.intellij.openapi.project.Project;
+import com.intellij.openapi.application.ApplicationManager;
 import com.intellij.openapi.wm.ToolWindow;
 import com.intellij.openapi.wm.ToolWindowId;
 import com.intellij.openapi.wm.ToolWindowManager;
-import com.intellij.psi.PsiDocumentManager;
 import com.intellij.ui.components.JBScrollPane;
 import com.intellij.ui.content.Content;
 import com.intellij.ui.content.ContentFactory;
@@ -22,20 +19,25 @@ public class MessageViewUtil {
         resultTextArea.setLineWrap(true);
         resultTextArea.setWrapStyleWord(true);
         JBScrollPane scrollPane = new JBScrollPane(resultTextArea);
+        ProgressIndicatorUtil.getInstance().updateProgress(80, "Packing data...");
 
-        Document currentDoc = FileEditorManager.getInstance(ModuleProjectUtil.getInstance().getProject()).getSelectedTextEditor().getDocument();
-        String fileName = PsiDocumentManager.getInstance(ModuleProjectUtil.getInstance().getProject()).getPsiFile(currentDoc).getOriginalFile().getVirtualFile().getName();
-        ModuleProjectUtil.getInstance().getProject().getService(MessageView.class);
-        ToolWindow toolWindow = ToolWindowManager.getInstance(ModuleProjectUtil.getInstance().getProject()).getToolWindow(ToolWindowId.MESSAGES_WINDOW);
-        Content content =  toolWindow.getContentManager().findContent(fileName);
-        boolean fileNameContentDoesNotExists = content == null;
-        ContentFactory contentFactory = ContentFactory.SERVICE.getInstance();
-        if (!fileNameContentDoesNotExists) {
-            content.getManager().removeContent(content, true);
-            toolWindow.getContentManager().addContent(contentFactory.createContent(scrollPane, fileName, false));
-        } else {
-            toolWindow.getContentManager().addContent(contentFactory.createContent(scrollPane, fileName, false));
-        }
-        toolWindow.activate(null);
+        String fileName = ModuleProjectUtil.getInstance().getModuleProject(Thread.currentThread().getId()).getCurrentDocumentPSIFile().getOriginalFile().getVirtualFile().getName();
+
+        ProgressIndicatorUtil.getInstance().updateProgress(90, "Prepare to display data...");
+        ApplicationManager.getApplication().invokeLaterOnWriteThread(() -> {
+            ModuleProjectUtil.getInstance().getModuleProject(Thread.currentThread().getId()).getProject().getService(MessageView.class);
+            ToolWindow toolWindow = ToolWindowManager.getInstance(ModuleProjectUtil.getInstance().getModuleProject(Thread.currentThread().getId()).getProject()).getToolWindow(ToolWindowId.MESSAGES_WINDOW);
+            Content content =  toolWindow.getContentManager().findContent(fileName);
+            boolean fileNameContentDoesNotExists = content == null;
+            ContentFactory contentFactory = ContentFactory.SERVICE.getInstance();
+            if (!fileNameContentDoesNotExists) {
+                content.getManager().removeContent(content, true);
+                toolWindow.getContentManager().addContent(contentFactory.createContent(scrollPane, fileName, false));
+            } else {
+                toolWindow.getContentManager().addContent(contentFactory.createContent(scrollPane, fileName, false));
+            }
+            toolWindow.activate(null);
+        });
+        ProgressIndicatorUtil.getInstance().updateProgress(95, "Process finished");
     }
 }
