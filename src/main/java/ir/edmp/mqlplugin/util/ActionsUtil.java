3e7d78ffb37f62783fd78637e20974362853382d
m.parsapour <m.parsapour@plm.ir>
2024-03-07 04:21:17 +0500

    Update ActionsUtil to build and return new ModuleProject

--- src/main/java/ir/edmp/mqlplugin/util/ActionsUtil.java
+++ src/main/java/ir/edmp/mqlplugin/util/ActionsUtil.java
@@ -5,20 +5,36 @@ import com.intellij.openapi.editor.Document;
 import com.intellij.openapi.editor.Editor;
 import com.intellij.openapi.fileEditor.FileEditorManager;
 import com.intellij.openapi.project.Project;
+import com.intellij.openapi.roots.ProjectRootManager;
 import com.intellij.psi.PsiDocumentManager;
+import com.intellij.psi.PsiFile;
+import ir.edmp.mqlplugin.builder.ModuleProjectBuilder;
+import ir.edmp.mqlplugin.entity.ModuleProject;
 
 public class ActionsUtil {
 
-    public static boolean noEditorFound(AnActionEvent event) {
-        Project moduleProject = event.getProject();
-        ModuleProjectUtil.getInstance().setProject(moduleProject);
-        Editor editor = FileEditorManager.getInstance(moduleProject).getSelectedTextEditor();
+    public static boolean noEditorFound() {
+        Editor editor = ModuleProjectUtil.getInstance().getModuleProject(Thread.currentThread().getId()).getEditor();
         return editor == null;
     }
 
-    public static String getFileExtension(AnActionEvent event) {
-        Editor editor = FileEditorManager.getInstance(ModuleProjectUtil.getInstance().getProject()).getSelectedTextEditor();
+    public static String getFileExtension() {
+        return ModuleProjectUtil.getInstance().getModuleProject(Thread.currentThread().getId()).getCurrentDocumentPSIFile().getOriginalFile().getVirtualFile().getExtension();
+    }
+
+    public static ModuleProject setModuleData(AnActionEvent event) {
+        Project project = event.getProject();
+        Editor editor = FileEditorManager.getInstance(project).getSelectedTextEditor();
         Document currentDoc = editor.getDocument();
-        return PsiDocumentManager.getInstance(ModuleProjectUtil.getInstance().getProject()).getPsiFile(currentDoc).getOriginalFile().getVirtualFile().getExtension();
+        PsiFile currentDocPsiFile = PsiDocumentManager.getInstance(project).getPsiFile(currentDoc);
+        ModuleProject moduleProject = ModuleProjectBuilder.getInstance()
+                .setProject(project)
+                .setEvent(event)
+                .setCurrentDocument(currentDoc)
+                .setCurrentDocumentPSIFile(currentDocPsiFile)
+                .setEditor(editor)
+                .setModuleName(ProjectRootManager.getInstance(project).getFileIndex().getModuleForFile(PsiDocumentManager.getInstance(project).getPsiFile(currentDoc).getVirtualFile()).getName())
+                .build();
+        return moduleProject;
     }
 }
